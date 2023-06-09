52d2088ddf26afd76386b05661dd1c91e1a0e2b5
ellyyyyyy <dimanuha11@mail.ru>
2023-06-09 16:21:07 +0300

    initial

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Твоя.Музыка</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="header">
+        <div id="inner-header">
+            <div class="wrap">
+                <a href="/" class="logo"><img src="fonts/logo.png" alt="ТВОЯ МУЗЫКА"></a>
+                <ul class="header-menu">
+                    <li><a href="/rating"><span>Рейтинг</span> </a></li>
+                    <li><a href="/artists"><span>Артисты</span> </a></li>
+                    <li><a href="/add-artist"><span>Добавить артиста</span> </a></li>
+                    <li><a href="/rate-song"><span>Оценить трек</span> </a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</body>
+<script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+</html>
